cd01843fed2d6a212d6d398a0bf9a34f648b6a2c
Potterfer <99811010+Potterfer@users.noreply.github.com>
2022-02-23 07:50:58 -0500

    Add files via upload

---
+++ controladorTienda.js
@@ -0,0 +1,33 @@
+//Entradas del problema
+/*
+
+Comentario de bloque
+
+*/
+//ENTRADAS==VARIABLES==CONSTANTES
+
+let nombreProducto='Libros Saga Completa'
+let precioProducto=700000
+let pesoProducto=500
+let tieneDescuento=true
+let descripcionProducto="Libros Originales edicion Salamandra del 1 al 10"
+let telVendedor="3177201647"
+
+
+const NOMBRE_CLIENTE="Maria Fernanda"
+
+// arreglos unidimensionales (vectores)
+// sirven para almacenar varios elementos en 1 sola variable
+let productos=['Libros,Camiseta,Morral']
+let productos2=Array("Libros,Camiseta,Morral") 
+
+
+
+
+//"SALIDAS
+console.log("Buenas")
+console.log(nombreProducto)
+console.log("Su nombre es: " +NOMBRE_CLIENTE+ " y su producto es: "+nombreProducto)
+console.log(`su nombre es: ${NOMBRE_CLIENTE} y su producto es: ${nombreProducto}`)
+console.log(productos)
+console.log(productos2)
